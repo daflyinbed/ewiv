16d01dd2aac721a087adc1d9a57c85d996900bf6
夕舞八弦 <1677759063@qq.com>
2019-06-16 17:54:06 +0800

    load page from wiki

--- extension.js
+++ extension.js
@@ -1,7 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
+const MWBot = require('mwbot');
+const request = require('request');
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -9,12 +10,32 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	const MWBot = require('mwbot');
+	async function init(){
+		let page = await vscode.window.showInputBox({
+			value: '',
+			ignoreFocusOut: true,
+			password: false,
+			prompt: '要编辑的页面'
+		});
+		let indexurl = vscode.workspace.getConfiguration().get('ewiv.indexUrl');
+		console.log('https://' + indexurl + '?title=' + page + '&action=raw');
+		request('https://' + indexurl + '?title=' + encodeURI(page)+'&action=raw',function(error,response,body){
+			if(!error&&response.statusCode==200){
+				vscode.window.activeTextEditor.edit(editBuilder => {
+					const end = new vscode.Position(vscode.window.activeTextEditor.document.lineCount + 1, 0);
+					vscode.languages.setTextDocumentLanguage(vscode.window.activeTextEditor.document, 'wikitext');
+					const text = body;
+					editBuilder.replace(new vscode.Range(new vscode.Position(0, 0), end), text);
+				});
+			}
+		})
+		
+	}
 	let CommandEwivPush = vscode.commands.registerCommand('extension.ewivpush', function () {
+		let bot = new MWBot();
 		if (vscode.window.activeTextEditor) {
 			let content = vscode.window.activeTextEditor.document.getText();
 			console.log(content);
-			let bot = new MWBot();
 			let apiurl = vscode.workspace.getConfiguration().get('ewiv.apiUrl');
 			let username = vscode.workspace.getConfiguration().get('ewiv.userName');
 			let password = vscode.workspace.getConfiguration().get('ewiv.password');
@@ -38,8 +59,15 @@ function activate(context) {
 			console.error("undefined vscode.window.activeTextEditor");
 		}
 	});
+
+	let CommandEwivInit = vscode.commands.registerCommand('extension.ewivinit',function () {
+		init();
+	});
+	context.subscriptions.push(CommandEwivInit);
 	context.subscriptions.push(CommandEwivPush);
 }
+
+ 
 exports.activate = activate;
 			
 // this method is called when your extension is deactivated
